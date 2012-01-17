207661d4c645ee5b4abc613a71c608e61c468d3c
gu1dry <markus.guidry@gmail.com>
2012-01-16 22:14:21 -0500

    Initial commit.

---
+++ src/com/andrew/music/template/main.java
@@ -0,0 +1,14 @@
+package com.andrew.music.template;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.andrew.music.template.R;
+
+public class main extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
